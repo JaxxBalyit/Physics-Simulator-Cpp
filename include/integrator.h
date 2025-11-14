45bdf9e4572639fcc8081d377e2c29159615cbf6
José Emmanuel Chávez Zepeda <je.chavezzepeda@ugto.mx>
2025-11-13 21:52:55 -0600

    Update integrator header

--- include/integrator.h
+++ include/integrator.h
@@ -4,5 +4,6 @@
 #include "physics.h"
 
 State eulerStep(const State& s, double m, double rho, double Cd, double A, double dt);
+State rk4Step(const State& s, const Params& p, double dt);
 
 #endif // INTEGRATOR_H
