30d02a2272d6f027e8b9ff928473cb5aab133380
José Emmanuel Chávez Zepeda <je.chavezzepeda@ugto.mx>
2025-11-13 22:50:05 -0600

    Fix main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
 #include "simulation.h"
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
-    if (argc < 2 || strcmp(argv[1], "--help")==0) {
+    // Help or missing main argument
+    if (argc < 2 || strcmp(argv[1], "--help") == 0) {
         std::cerr << "Usage: ./PhysSim <projectile|pendulum|oscillator> [--integrator=euler|rk4]\n";
         return 1;
     }
-    
+
     std::string system = argv[1];
-    runSimulation(system);
+    std::string integrator = "euler"; // default
+
+    // Optional second argument
+    if (argc >= 3) {
+        std::string arg = argv[2];
+
+        // Expected format: --integrator=...
+        std::string prefix = "--integrator=";
+        if (arg.rfind(prefix, 0) == 0) {
+            integrator = arg.substr(prefix.size());
+
+            if (integrator != "euler" && integrator != "rk4") {
+                std::cerr << "Error: integrator must be 'euler' or 'rk4'.\n";
+                return 1;
+            }
+        }
+        else {
+            std::cerr << "Unknown option: " << arg << "\n";
+            return 1;
+        }
+    }
+
+    runSimulation(system, integrator);
     return 0;
 }
